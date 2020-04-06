d359a854030e110448e5fca9b25331616008d0f3
dunz <zergcake@gmail.com>
2020-04-06 11:01:05 +0900

    update husky

--- husky.config.js
+++ husky.config.js
@@ -11,7 +11,7 @@ module.exports = {
     'hooks': {
         // 'pre-commit': 'scripts/executeTest.sh && npm run test:unit && exit 1'
         // 'pre-commit': 'scripts/executeTest.sh && lint-staged && exit 1'
-        'pre-commit': 'npm run test:unit --findRelatedTests --lastCommit'
+        'pre-push': 'npm run test:unit --findRelatedTests'
         // 'pre-commit': 'lint-staged'
     }
 };
